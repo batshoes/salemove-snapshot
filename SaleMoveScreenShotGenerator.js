f8edf7bf9ea34eb28661e915b8457fc52ff6235f
James Middlemiss <middlemissj.usa@gmail.com>
2016-04-27 13:17:07 -0400

    adds 'scrolling' mechanism, cleans up some nesting

--- SaleMoveScreenShotGenerator.js
+++ SaleMoveScreenShotGenerator.js
@@ -1,7 +1,6 @@
 $('document').ready(function(){
   $.getScript("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/hmac-sha1.js", function(){
     $('#pushCookie').on('click', saveCookie);
-
     $body = $("body");
     $body.addClass("loading");
     requestUrlBox()
@@ -14,7 +13,7 @@ function requestUrlBox(){
                   "\x34\x63\x31\x62\x65\x61\x34\x34\x2D\x61\x39\x66\x38\x2D\x34\x38\x62\x64\x2D\x62\x36\x31\x63\x2D\x61\x35\x65\x35\x65\x62\x62\x32\x30\x31\x66\x66" ];
   getKeys(_0x1b89)
 
-  var userWebsite = getCookie(cookieName) 
+  var userWebsite = getCookie(cookieName)
       options = {
           url: userWebsite,
           full_page: true,
@@ -23,29 +22,55 @@ function requestUrlBox(){
         }
       queryString = $.param(options)
                 .toString();
-      console.log(queryString)
       token = CryptoJS.HmacSHA1(queryString, api_secret)
       callUrl = 'https://api.urlbox.io/v1/' + api + '/' + token + '/png/?' + queryString + "/"
 
-  $('#image').attr("src", callUrl )
+  $('.image').attr("src", callUrl )
                .css("display", "none")
 
   var loadingTimeout = setTimeout(function(){
     $body.removeClass("loading");
-    $('#image').attr("src", "https://learn.getgrav.org/user/pages/11.troubleshooting/01.page-not-found/error-404.png" )
+    $('.image').attr("src", "https://learn.getgrav.org/user/pages/11.troubleshooting/01.page-not-found/error-404.png" )
                .css("display", "inline-block")
-  }, 20000)
+    $('#left').css('display', 'none');
+  }, 30000)
 
-  $('#image').on('load', function(response){
+  $('.image').on('load', function(response){
     clearTimeout(loadingTimeout);
     updateSlack(userWebsite);
 
     $body.removeClass("loading");
-    $('#image').css('display', 'inline-block');
+    $('.image').css('display', 'inline-block');
+    $('#left').css('display', 'inline-block');
+
+    setTimeout(function(){
+      $('#top').trigger("click");
+      setInterval(function(){
+        $('#top').trigger("click");
+      }, 10000);
+    }, 1000);
+
+    setTimeout(function(){
+      $('#bottom').trigger("click");
+      setInterval(function(){
+        $('#bottom').trigger("click");
+      }, 10000);
+    }, 11000)
   })
 
   api = "obscuredKey"
   api_secret = "obscuredKey"
+
+  $('a[href^="#"]').on('click', function(event) {
+      var target = $( $(this).attr('href') );
+
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: target.offset().top
+          }, 18000);
+      }
+    });
 } 
 
 cookieName = "websiteUrl";
